3332b8d652565ff4248161139caeaf88a06d86b3
boblee <boblee2000@outlook.com>
2017-10-14 21:46:00 -0400

    update

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 
 a = y'*log(sigmoid(theta'*X'))';
 b = (1-y')*log(1 - sigmoid(theta'*X'))';
-tmp = theta 
+tmp = theta; 
 tmp(1:1,:) = [0];
 J = ((-1)/m)*(a+b) + (lambda*(tmp'*tmp))/(2*m);
 
