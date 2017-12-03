a794b1c89ad7267d17029993dfc9fbb66e96ddb5
boblee <boblee2000@outlook.com>
2017-12-03 13:04:40 -0500

    submit test3,4

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,7 +40,10 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = sum(sum((X*Theta'.*R - Y).^2))/2;
 
+X_grad = (X*Theta'.*R - Y)*Theta;
+Theta_grad = (X*Theta'.*R - Y)'*X;
 
 
 
